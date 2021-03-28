f126a91dd418bb0c937f20ab335106eaaa3e4c3e
Cat-Denton <willwdenton@gmail.com>
2021-03-28 16:57:44 -0700

    add getSearch method
    Co-authored-by: Sofia Acosta <sofiaacostarascon@gmail.com>
    Co-authored-by: Faisal Rana <inquisitive@gmail.com>
    Co-authored-by: Karlson Drendel <kdrendel99@gmail.com>

--- src/spotify-service.js
+++ src/spotify-service.js
@@ -17,5 +17,22 @@ export default class SpotifyService {
     } catch(error) {
       return error.message
     }
+
+  }
+  static async getSearch(token) {
+    try {
+      const result = await fetch('https://api.spotify.com/v1/search?q=year%3A1994&type=track', {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer' + token}
+      
+      });
+     if (!result) {
+       throw Error(result.statusText)
+     }
+     return result.json()  
+    } catch(error){
+      return error.message
+    }
   }
 }
+
