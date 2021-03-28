b972b61a82b33734a7df3ba9d0ff120934ad80e6
Cat-Denton <willwdenton@gmail.com>
2021-03-28 15:24:26 -0700

    add testToken() function to store authentication token from getToken method
    Co-authored-by: Sofia Acosta <sofiaacostarascon@gmail.com>
    Co-authored-by: Karlson Drendel <kdrendel99@gmail.com>
    Co-authored-by: Faisal Rana <inquisitive@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import SpotifyService from './spotify-service.js';
 
+async function testToken() {
+  const token = await SpotifyService.getToken();
+  console.log(token);
+}
+
+testToken();
 
