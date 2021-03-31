c09846b3bc46c5e2caa6028d0f39dbb37ceeda4d
Cat-Denton <willwdenton@gmail.com>
2021-03-30 20:40:01 -0700

    change spotify api endpoint URL to search for tracks from 1994

--- src/spotify-service.js
+++ src/spotify-service.js
@@ -22,7 +22,7 @@ export default class SpotifyService {
   static async getSearch(token) {
     try {
       debugger
-      const response = await fetch('https://api.spotify.com/v1/search?q=1994&type=track', {
+      const response = await fetch('https://api.spotify.com/v1/search?q=year%3A1994&type=track', {
         method: 'GET',
         headers: { 'Authorization' : 'Bearer ' + token}
       
