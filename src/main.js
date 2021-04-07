d353a3d7c2433734a5a81de6fa6b54766cd374a3
Karlson Drendel <kdrendel99@gmail.com>
2021-04-06 20:09:22 -0700

    Neon sign functionality

---
+++ src/main.js
@@ -0,0 +1,58 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import SpotifyService from './spotify-service.js';
+
+$(document).ready( async function() {
+  const token = await testToken();
+  const genreList = await SpotifyService.getGenres(token);
+  genreList.genres.forEach(function(element){
+    $("#inputGenre").append(`<option value=${element}>${element[0].toUpperCase() + element.substring(1)}</option>`);
+  });
+});
+$("#input-form").submit(function(event) {
+  clearFields();
+  const birthDate = $("#DOB").val();
+  const birthYear = parseInt(birthDate.slice(0,4));
+  const hsYears = [(birthYear)+14,(birthYear)+18];
+  const genre = $("#inputGenre").val();
+  outputSearch(genre,hsYears);
+  changeView();
+  event.preventDefault();
+});
+
+function changeView(){
+  $("#hiddenInputs").hide();
+  $("#hiddenOutput").show();
+}
+
+function getName(fullName){
+  let array = fullName.split(/[ ,]+/);
+  return array[0];
+}
+function clearFields() {
+  $("#songList").text("");
+}
+
+async function testToken() {
+  const token = await SpotifyService.getToken();
+  return token;
+}
+
+async function outputSearch(genre,years) {
+  const fullName = $("#name").val();
+  let name = getName(fullName);
+  const token = await testToken();
+  const search = await SpotifyService.getSearch(token,genre,years);
+  search.tracks.items.forEach(function(element) {
+    $('.list').text(`Hello ${name}, here is a list of your personalized songs from high school:`);
+    $("#songList").append("<li>" + element.name);
+  });
+} 
+
+
+
+
+
+
---
+++ src/spotify-service.js
@@ -0,0 +1,51 @@
+export default class SpotifyService {
+  static async getToken() {
+    try {
+      const response = await fetch ('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/x-www-form-urlencoded', 
+          'Authorization' : 'Basic ' + btoa(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`)
+        },
+        body: 'grant_type=client_credentials'
+      });
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      const token = await response.json();
+      return token.access_token;
+    } catch(error) {
+      return error.message;
+    }
+
+  }
+  static async getSearch(token,genre,years) {
+    try {
+      const response = await fetch(`https://api.spotify.com/v1/search?q=year%3A${years[0]}-${years[1]}%20genre%3A${genre}&type=track`, {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + token}
+      });
+     if (!response.ok) {
+        throw Error(response.statusText)
+      }
+     return await response.json()  
+    } catch(error){
+      return error.message
+    }
+  }
+  static async getGenres(token) {
+    try {
+      const response = await fetch(`https://api.spotify.com/v1/recommendations/available-genre-seeds`, {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + token}
+      });
+     if (!response.ok) {
+       throw Error(response.statusText)
+     }
+     return await response.json()  
+    } catch(error){
+      return error.message
+    }
+  }
+}
+
